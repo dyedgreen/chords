475a038755e60dfe066684df1b755262b443cc3d
Tilman Roeder <dyed.green.info@gmail.com>
2020-01-07 22:02:41 +0000

    Solid first draft for generating chord algorithm
    Works ~ 60% of time or so; is a bit delicate
    may look into improving it with Bayes theorem
    later ...

--- audio.js
+++ audio.js
@@ -2,9 +2,8 @@
 async function getAudioStream() {
   const worker = new Worker("./audio_worker.js");
 
-  const bufferSize   = 4096; // smallest 2^N which has decent resolution ...
-  const minNote      = -22; // smallest note with unique bin
-  const maxNote      = 88-5*12; // highest key on piano
+  const bufferSize = 4096; // smallest 2^N which has decent resolution ...
+  const timeStep   = 100 - 1; // in ms
 
   // Create audio pipeline
   const AudioContext = window.AudioContext || window.webkitAudioContext;
@@ -16,8 +15,8 @@ async function getAudioStream() {
 
   // FFT settings
   fft.fftSize = bufferSize;
-  fft.smoothingTimeConstant = 0.2; // TODO: Adjust ...
-  fft.minDecibels = -80;//-58; // TODO: Adjust ...
+  fft.smoothingTimeConstant = 0.1; // TODO: Adjust ... (still)
+  fft.minDecibels = -70; // think it's good; this is a fine line to walk though ...
 
   // Connect audio nodes
   source.connect(fft);
@@ -26,122 +25,24 @@ async function getAudioStream() {
 
   // Buffer for fft results
   const buffer = new Uint8Array(fft.frequencyBinCount);
-
   const hzPerBin = (context.sampleRate) / (2*fft.frequencyBinCount);
-  function freqIndex(f) {
-    return parseInt((f + hzPerBin/2) / hzPerBin);
-  };
-  function indexFreq(i) {
-    return hzPerBin * i - hzPerBin/2;
-  }
 
-  // Generate frequencies at each bin and if they are a musical note
-  const freqs = new Float32Array(fft.frequencyBinCount);
-  for (let i = 0; i < fft.frequencyBinCount; i ++)
-    freqs[i] = indexFreq(i);
-
-  for (let n = -5*12; n < 88-5*12; n ++) {
-    const f = noteFreq(n);
-    const i = freqIndex(f);
-    console.log(n, i, f, freqs[i]);
-  }
-
-  const isNote = new Uint8Array(fft.frequencyBinCount);
-  const notes = new Int8Array(fft.frequencyBinCount);
-  for (let n = minNote; n < 88-5*12; n ++) {
-    const f = noteFreq(n);
-    isNote[freqIndex(f)] = 1;
-    notes[freqIndex(f)] = n;
-  }
-
-  for (let i = 0; i < freqs.length; i ++) {
-    if (!isNote[i])
-      continue;
-    console.log("2nd pass", i, notes[i], freqs[i], noteFreq(notes[i]));
-  }
-
-  // Original
-  function getPeaksSimple(buffer) {
-    const peaks = [];
-    let avg = buffer[0] + buffer[buffer.length-1];
-    for (let i = 1, max = buffer.length-1; i < max; i ++) {
-      if (buffer[i-1] < buffer[i] && buffer[i] > buffer[i+1])
-        peaks.push(i);
-      avg += buffer[i];
-    }
-    avg /= buffer.length;
-    console.log(peaks.filter(i => isNote[i]).map(i => [buffer[i], notes[i]]));
-    return peaks.filter(i => isNote[i] && buffer[i] > avg);
-  }
-
-  // Use double average as thresh-hold
-  // This gives reasonable results so far ...
-  function getPeaks(buffer) {
-    let peaks = [];
-    let avg = buffer[0] + buffer[buffer.length-1];
-
-    // Get peaks that are notes and average peak
-    for (let i = 1, max = buffer.length-1; i < max; i ++) {
-      if (buffer[i-1] < buffer[i] && buffer[i] > buffer[i+1] && isNote[i])
-        peaks.push(i);
-      avg += buffer[i];
-    }
-    avg /= buffer.length;
-
-    // Select those notes which have more than average power
-    peaks = peaks.filter(i => buffer[i] > avg);
-
-    // Select those peaks which higher than average peak amongst the peaks themselves
-    avg = 0;
-    for (let i of peaks)
-      avg += buffer[i];
-    avg /= peaks.length;
-    peaks = peaks.filter(i => buffer[i] > avg);
-
-    return peaks;
-  }
-
-  console.log(context);
-
-  let i = 0; // REMOVE ME
+  // Send settings to worker
+  worker.postMessage({
+    timeStep,
+    hzPerBin,
+    frequencyBinCount: buffer.length,
+  });
 
   // This is called when we have a new FFT frame available
+  let next = performance.now() - 1;
   processor.onaudioprocess = function(e) {
-    // Do something with the data, e.g. convert it to WAV
-    // const buff = e.inputBuffer; /* raw audio */
-    // console.log(buff.getChannelData(0));
     fft.getByteFrequencyData(buffer);
-    worker.postMessage(buffer);
-    // if ((i++) % 1 === 0) { // REMOVE ME
-    //   // console.log(buff);
-    //   console.log(buffer);
-    //   console.log(freqs);
-    //   console.log(getPeaks(buffer).map(i => {
-    //     return [noteName(notes[i]), notes[i], buffer[i]];
-    //   }));
-    // }
+    if (performance.now() > next) {
+      worker.postMessage(buffer);
+      next = performance.now() + timeStep;
+    }
   };
-}
-
-// Frequency of note, where n is number
-// of half-tone steps between A_4 and the
-// note.
-// E.g. C_3 <=> n=-10
-// Note: 88 key piano goes from
-// A_{-1} to C_7 or
-// n = -5*12 to n = 88-5*12-1
-function noteFreq(n) {
-  return 440 * Math.pow(2, n/12);
-}
-
-// Ahrg, I'm going crazy ...
-function noteName(n) {
-  const octave = (n >= 0 ? parseInt(Math.abs(n) / 12) : -1 - parseInt(Math.abs(n) / 12)) + 4;
-  // note from friend: seems like it will 'look good' if we maximize distance from lower note
-  //                   in letter system (?) but in principle, no right/wrong for single note
-  const letters = ['A','#A','B','C','#C','D','#D','E','F','#F','G','#G'];
-  // min n is -5*12
-  return `${letters[(5*12+n)%12]} (${octave})`;
 }
 
 getAudioStream();
--- audio_worker.js
+++ audio_worker.js
@@ -2,9 +2,251 @@
 
 console.log("Audio worker started");
 
-const buffer = [];
+const minNote    = -22; // smallest note with unique bin
+const maxNote    = 88-5*12; // highest key on piano
+const minPower   = 30; // check what is good ...
+const timeWindow = 10; // # of frames in time window
 
-// Collect spectra
-onmessage = function({data}) {
-  console.log(data);
+// Time buffer for notes
+const bufferOct = []; // contains octave relative to A_4.
+                      // i.e. nth index is m <-> note = n + 12 * m
+const bufferPow = []; // contains power for octaves
+for (let i = 0; i < timeWindow; i ++) {
+  bufferOct.push(new Int8Array(12));
+  bufferPow.push(new Uint8Array(12));
+}
+let bufferIdx = 0; // Current position in buffer
+clearBuffer();
+
+// Settings needed to be known
+let settings = {
+  timeStep: null,
+  hzPerBin: null,
+  frequencyBinCount: null,
+  isNote: null,
+  notes: null,
 };
+
+// Receive initial settings
+onmessage = function({data}) {
+  settings = data;
+  // Generate notes
+  notes = new Int8Array(settings.frequencyBinCount);
+  isNote = new Uint8Array(settings.frequencyBinCount);
+  for (let n = minNote; n < maxNote; n ++) {
+    notes[freqIndex(noteFreq(n))] = n;
+    isNote[freqIndex(noteFreq(n))] = 1;
+  }
+  settings.notes = notes;
+  settings.isNote = isNote;
+  onmessage = collect;
+};
+
+let num = 0; // DELETE ME
+
+// Collect new data point
+function collect({data}) {
+  detectPeaks(data);
+  if (bufferIdx === bufferOct.length)
+    finalizeBuffer();
+}
+
+// Edge detection algorithm - sketch (SINGLE CHORD, 1s timeout)
+// 1) if there suddenly are notes, fill a 1 second buffer (i.e. a 10 frame buffer)
+//    -> note: for time series collection, this needs to be rethought (!)
+//    -> (if time-series collection is possible ...)
+// 2) at each point in time in the buffer, record the highest power note at each key
+// 3) after the buffer is filled, select notes that have 2+ or 3+ entries
+// 4) report these notes as the chord
+
+// octave errors:
+// > To correct, apply this rule: if the second peak amplitude below
+// > initially chosen pitch is approximately 1/2 of the chosen pitch
+// > AND the ratio of amplitudes is above a threshold (e.g., 0.2 for 5
+// > harmonics), THEN select the lower octave peak as the pitch for
+// > the current frame.
+//
+// select lowest if the next one is ~0.5 of previous
+
+// Zero powers in octave buffer
+function clearBuffer() {
+  for (let i = 0; i < bufferOct.length; i ++) {
+    bufferPow[i].fill(0);
+  }
+  bufferIdx = 0;
+}
+
+// Detect peaks, write to buffer if any detected
+// or buffer has data. The main caller has to finalize
+// the buffer whenever it is completely full.
+function detectPeaks(spectrum) {
+  // Find average spectrum
+  let avg = 0;
+  for (let i = 0; i < spectrum.length; i ++) {
+    avg += spectrum[i];
+  }
+  avg /= spectrum.length;
+
+  // Notes above average spectrum are set in buffer (todo: optimize this ...)
+  let added = false;
+  for (let i = 1; i < spectrum.length-1; i ++) {
+    if (!settings.isNote[i])
+      continue;
+
+    const pow = spectrum[i];
+    if (pow < avg + 10) // *magic number 10* (TODO: twiddle ...)
+      continue;
+
+    // Not a peak ...
+    if (spectrum[i-1] >= spectrum[i] || spectrum[i] <= spectrum[i+1])
+      continue;
+
+    // Determine note
+    const n = settings.notes[i];
+    const key = (120 + n) % 12;
+
+    // If peak bigger than current note in buffer, write to buffer
+    if (bufferPow[bufferIdx][key] < pow * 1.2) {
+      bufferPow[bufferIdx][key] = pow;
+      bufferOct[bufferIdx][key] = n >= 0 ? parseInt(n / 12) : parseInt(n / 12) - 1; // relative to A_4
+      added = true;
+    }
+  }
+
+  // If notes were added, or buffer is not empty
+  // fill up the buffer
+  if (added || bufferIdx > 0)
+    ++bufferIdx;
+}
+
+// Extract chord from full buffer. This uses
+// (big) discretized time-steps. Later, I want
+// to experiment with reporting notes continuously.
+function finalizeBuffer() {
+  // Debug output: select every note found in spectrum + how often is is seen
+  const powers = finalizeBuffer._power_map;
+  powers.clear();
+
+  for (let idx = 0; idx < bufferOct.length; idx ++) {
+    for (let key = 0; key < 12; key ++) {
+      const n = key + 12 * bufferOct[idx][key];
+      const total = powers.get(n) | 0;
+      powers.set(n, total + bufferPow[idx][key]);
+    }
+  }
+
+  const res = [];
+  for (const [n, pow] of powers)
+    res.push([noteName(n), n, pow]);
+
+  let avg = 0;
+  for (let i = 0; i < res.length; i ++)
+    avg += Math.pow(res[i][2], 2); // raised to 2nd /!\
+  avg /= res.length;
+
+  if (avg > 10_000) {
+    // Accept chord
+    console.log(avg);
+    console.log("RAW:", res);
+    console.log(res.filter(([,,pow]) => Math.pow(pow, 2) > avg));
+  }
+
+  clearBuffer();
+}
+finalizeBuffer._power_map = new Map();
+
+// Edge detection (CONTINOUS, EMITS NOTES)
+// todo (...) -> problem, data is too unreliable at the moment :(
+
+// Note: Maybe use Bayesian / other simple probabilistic model which
+//       predicts if a signal is correct/ fake based on previous points,
+//       neighboring points. This might mask the errors well enough to do
+//       continuous analysis ...
+//       (this might then also allow to capture multiple tones simultaneously!)
+//
+// SIGNALS for Bayes rule:
+//  - relative power (higher power -> more likely)
+//  - number of occurrences (occurred in previous frames -> more likely)
+//  - maybe power is monotonously decreasing -> more likely (as the chords ring out ...)
+//  - relative power to other tones of same type (to remove overtones)
+//
+// however, the above algorithm seems to be a good first draft
+
+// Random stuff from before
+// (some this analysis might be moved to C once it works ...)
+
+function freqIndex(f) {
+  return parseInt((f + settings.hzPerBin/2) / settings.hzPerBin);
+};
+function indexFreq(i) {
+  return settings.hzPerBin * i - settings.hzPerBin/2;
+}
+
+// Use double average as thresh-hold
+// This gives reasonable results so far ...
+function _____getPeaks(buffer) {
+  let peaks = [];
+  let avg = buffer[0] + buffer[buffer.length-1];
+
+  // Get peaks that are notes and average peak
+  for (let i = 1, max = buffer.length-1; i < max; i ++) {
+    if (settings.isNote[i] && buffer[i] > minPower && buffer[i-1] < buffer[i] && buffer[i] > buffer[i+1])
+      peaks.push(i);
+    avg += buffer[i];
+  }
+  avg /= buffer.length;
+
+  // Select those notes which have more than average power
+  peaks = peaks.filter(i => buffer[i] > avg);
+
+  // Select those peaks which higher than average peak amongst the peaks themselves
+  avg = 0;
+  for (let i of peaks)
+    avg += buffer[i];
+  avg /= peaks.length;
+  peaks = peaks.filter(i => buffer[i] > avg);
+
+  return removeOvertones(buffer, peaks);
+}
+
+// Remove overtones from peaks list
+// this is quite slow at the moment...
+// -> removes any notes played with non-max power
+const _removeOvertones_map = new Map();
+const _removeOvertones_res = new Set();
+function ______removeOvertones(buffer, peaks) {
+  const res = _removeOvertones_res;
+  const seen = _removeOvertones_map;
+  seen.clear();
+  res.clear();
+  for (let i = 0; i < peaks.length; i ++) {
+    // make this better ...
+    const n = (120 + settings.notes[peaks[i]]) % 12;
+    if (seen.has(n) && seen.get(n) > buffer[peaks[i]])
+      continue;
+    seen.set(n, buffer[peaks[i]]);
+    res.add(peaks[i]);
+  }
+  return [...res];
+}
+
+// Frequency of note, where n is number
+// of half-tone steps between A_4 and the
+// note.
+// E.g. C_3 <=> n=-10
+// Note: 88 key piano goes from
+// A_{-1} to C_7 or
+// n = -5*12 to n = 88-5*12-1
+function noteFreq(n) {
+  return 440 * Math.pow(2, n/12);
+}
+
+// Ahrg, I'm going crazy ...
+function noteName(n) {
+  const octave = (n >= 0 ? parseInt(Math.abs(n) / 12) : -1 - parseInt(Math.abs(n) / 12)) + 4;
+  // note from friend: seems like it will 'look good' if we maximize distance from lower note
+  //                   in letter system (?) but in principle, no right/wrong for single note
+  const letters = ['A','#A','B','C','#C','D','#D','E','F','#F','G','#G'];
+  // min n is -5*12
+  return `${letters[(5*12+n)%12]} (${octave})`;
+}
